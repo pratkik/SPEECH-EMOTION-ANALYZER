a95745fe516b54fe6e4158032c098bd0fe93c3e4
pratkik <86764431+pratkik@users.noreply.github.com>
2022-09-26 16:39:29 +0530

    Update code.py

--- code.py
+++ code.py
@@ -28,3 +28,104 @@ print(mylist[400][6:-16])
 
 
 # Plotting the audio file's waveform and its spectrogram
+data, sampling_rate = librosa.load('RawData/f11 (2).wav')
+% pylab inline
+import os
+import pandas as pd
+import librosa
+import glob 
+
+plt.figure(figsize=(15, 5))
+librosa.display.waveplot(data, sr=sampling_rate)
+import matplotlib.pyplot as plt
+import scipy.io.wavfile
+import numpy as np
+import sys
+
+
+sr,x = scipy.io.wavfile.read('RawData/f10 (2).wav')
+
+## Parameters: 10ms step, 30ms window
+nstep = int(sr * 0.01)
+nwin  = int(sr * 0.03)
+nfft = nwin
+
+window = np.hamming(nwin)
+
+## will take windows x[n1:n2].  generate
+## and loop over n2 such that all frames
+## fit within the waveform
+nn = range(nwin, len(x), nstep)
+
+X = np.zeros( (len(nn), nfft//2) )
+
+for i,n in enumerate(nn):
+    xseg = x[n-nwin:n]
+    z = np.fft.fft(window * xseg, nfft)
+    X[i,:] = np.log(np.abs(z[:nfft//2]))
+
+plt.imshow(X.T, interpolation='nearest',
+    origin='lower',
+    aspect='auto')
+
+plt.show()
+
+#Setting the labels
+
+feeling_list=[]
+for item in mylist:
+    if item[6:-16]=='02' and int(item[18:-4])%2==0:
+        feeling_list.append('female_calm')
+    elif item[6:-16]=='02' and int(item[18:-4])%2==1:
+        feeling_list.append('male_calm')
+    elif item[6:-16]=='03' and int(item[18:-4])%2==0:
+        feeling_list.append('female_happy')
+    elif item[6:-16]=='03' and int(item[18:-4])%2==1:
+        feeling_list.append('male_happy')
+    elif item[6:-16]=='04' and int(item[18:-4])%2==0:
+        feeling_list.append('female_sad')
+    elif item[6:-16]=='04' and int(item[18:-4])%2==1:
+        feeling_list.append('male_sad')
+    elif item[6:-16]=='05' and int(item[18:-4])%2==0:
+        feeling_list.append('female_angry')
+    elif item[6:-16]=='05' and int(item[18:-4])%2==1:
+        feeling_list.append('male_angry')
+    elif item[6:-16]=='06' and int(item[18:-4])%2==0:
+        feeling_list.append('female_fearful')
+    elif item[6:-16]=='06' and int(item[18:-4])%2==1:
+        feeling_list.append('male_fearful')
+    elif item[:1]=='a':
+        feeling_list.append('male_angry')
+    elif item[:1]=='f':
+        feeling_list.append('male_fearful')
+    elif item[:1]=='h':
+        feeling_list.append('male_happy')
+    #elif item[:1]=='n':
+        #feeling_list.append('neutral')
+    elif item[:2]=='sa':
+        feeling_list.append('male_sad')
+labels = pd.DataFrame(feeling_list)
+labels[:10]
+
+#Getting the features of audio files
+
+
+df = pd.DataFrame(columns=['feature'])
+bookmark=0
+for index,y in enumerate(mylist):
+    if mylist[index][6:-16]!='01' and mylist[index][6:-16]!='07' and mylist[index][6:-16]!='08' and mylist[index][:2]!='su' and mylist[index][:1]!='n' and mylist[index][:1]!='d':
+        X, sample_rate = librosa.load('RawData/'+y, res_type='kaiser_fast',duration=2.5,sr=22050*2,offset=0.5)
+        sample_rate = np.array(sample_rate)
+        mfccs = np.mean(librosa.feature.mfcc(y=X, 
+                                            sr=sample_rate, 
+                                            n_mfcc=13),
+                        axis=0)
+        feature = mfccs
+        #[float(i) for i in feature]
+        #feature1=feature[:135]
+        df.loc[bookmark] = [feature]
+        bookmark=bookmark+1        
+df[:5]
+
+
+
